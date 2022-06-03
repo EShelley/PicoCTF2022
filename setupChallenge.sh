e9938d4ac0594822a95f6de2361b14daacd52f39
EShelley <eric@hireth.tech>
2022-06-03 11:49:03 -0500

    Updated setupChallenge.sh to be more robust - auto create parent Directories as necessary and place new challenge in correct directory

--- setupChallenge.sh
+++ setupChallenge.sh
@@ -1,17 +1,21 @@
 #! /bin/bash
 
-if ! [ -d "./$1"]; then
-	echo "Setting up new CTF challenge Folder for: " $1
+if   [ $# -eq 2 ]; then
+if ! [ -d "./$1/$2"]; then
+	echo "Setting up new CTF challenge Folder for: " $2
 	echo "======================================================================"
 	echo "Create Directory"
-	mkdir $1
+	mkdir ./$1/$2 -p
 	echo "-Done-"
 	echo "Create Dummy readme.md and flag files"
-	cat ./readme_template.md >  $1/README.md
-	sed -i 's/<CName>/'$1'/' $1/README.md
-	touch $1/flag
+	cat ./readme_template.md >  ./$1/$2/README.md
+	sed -i 's/CName/'$2'/' ./$1/$1/README.md
+	touch ./$1/$2/flag
 	
 	echo "Exiting"
 else
-	echo $1 " is already a directory - try again"
+	echo $1/$2 " is already a directory - try again"
+fi
+else
+	echo "./setupChallenge.sh <Challenge Name> <Category>"
 fi
